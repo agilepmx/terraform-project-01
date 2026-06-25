c80cf1f283299c62eaf1a59b060e6e0405463e8f
Basharalhusein <bashar@agilepmx.com>
2026-06-25 22:38:20 +0400

    Add s3 backend for tfstate file

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -5,6 +5,14 @@ terraform {
         version = ">= 6.0.0"
     }
   }
+
+  backend "s3" {
+  bucket         = "terraform-308281189096-us-east-1-an"
+  key            = "terraform/project-01/terraform.tfstate"
+  region         = "us-east-1"
+  encrypt        = true
+  use_lockfile  = true
+}
 }
 provider "aws" {
   region = "us-east-1"
