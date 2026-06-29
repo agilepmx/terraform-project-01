962b4a3207b7b2c50c0b8fb220169bf56c3de5e1
Basharalhusein <bashar@agilepmx.com>
2026-06-29 20:43:38 +0400

    Fix some issues and creat buckets using `terragrunt --backent-bootstrap`

--- environments/dev/us-east-1/VPC/output.tf
+++
@@ -1,4 +0,0 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = module.vpc_network.vpc_id
-}
--- modules/vpc_network/output.tf
+++ modules/vpc_network/output.tf
@@ -2,3 +2,38 @@ output "vpc_id" {
   description = "The ID of the VPC"
   value       = aws_vpc.this.id
 }
+
+output "vpc_cidr" {
+  description = "The IPv4 CIDR block of the VPC"
+  value       = aws_vpc.this.cidr_block
+}
+
+output "vpc_ipv6_cidr" {
+  description = "The IPv6 CIDR block of the VPC"
+  value       = aws_vpc.this.ipv6_cidr_block
+}
+
+output "igw_id" {
+  description = "The ID of the Internet Gateway"
+  value       = aws_internet_gateway.this.id
+}
+
+output "subnet_ids" {
+  description = "Map of subnet name to subnet ID"
+  value       = { for k, v in aws_subnet.this : k => v.id }
+}
+
+output "public_subnet_ids" {
+  description = "Map of public subnet name to subnet ID"
+  value       = { for k, v in aws_subnet.this : k => v.id if var.subnets_data[k].public == true }
+}
+
+output "private_subnet_ids" {
+  description = "Map of private subnet name to subnet ID"
+  value       = { for k, v in aws_subnet.this : k => v.id if var.subnets_data[k].public == false }
+}
+
+output "public_route_table_id" {
+  description = "The ID of the public route table"
+  value       = aws_route_table.public-rt.id
+}
